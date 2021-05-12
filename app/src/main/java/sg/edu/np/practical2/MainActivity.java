90c4420dfb984e9b7efbc3c34853ba3fd1679251
DaveTanKY <dave21sg@gmail.com>
2021-05-13 02:46:21 +0800

    fixing id and added dependencies

--- app/src/main/java/sg/edu/np/practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/practical2/MainActivity.java
@@ -1,19 +1,14 @@
 package sg.edu.np.practical2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         
         TextView name = findViewById(R.id.nameTxt);
         TextView desc = findViewById(R.id.descriptionTxt);
-        Button follow = findViewById(R.id.followBtn);
+        Button follow = findViewById(R.id.btnFollow);
         Button message = findViewById(R.id.messageBtn);
 
 //        user newUser = new user("Dave", "Description about Dave", 1, false, 20);
